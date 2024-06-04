8381ceb5d56cfa114425b41d56d12471b3b7e6c6
Mag1cMan <kimmy10290@hotmail.com>
2024-06-05 02:26:35 +0700

    Try Fix Montery Don't Show

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,11 +1,7 @@
 import GameEngine from 'components/game/GameEngine';
 const Game = () => {
   return ( 
-
-    <div className='ml-28'>
-
-        < GameEngine/>    
-    </div>
+      <GameEngine/>    
   );
 };
 
